f16cdbc332698efeef4359100216aad739c7df5b
Kofroň Adam <kofron@zvvz.cz>
2025-12-03 16:15:56 +0100

    moje rezervace

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import Message from './Message';
-import './modul.css';
+import { Outlet, useNavigate } from "react-router-dom";
 
-function App() {
+export default function App() {
+  const navigate = useNavigate();
+
   return (
-    <div><Message /></div>
+    <div className="header">
+      {/* Navigační lišta */}
+      <nav className="bg-white shadow-sm p-4 flex items-center gap-4">
+        <button
+          className="button:hover"
+          onClick={() => navigate("/")}
+        >
+          Rezervace
+        </button>
+        <button
+          className="button:hover"
+          onClick={() => navigate("/moje-rezervace")}
+        >
+          Moje rezervace
+        </button>
+        {/* Přidejte další odkazy dle potřeby */}
+      </nav>
+      {/* Obsah podle aktuální cesty */}
+      <main>
+        <Outlet />
+      </main>
+    </div>
   );
 }
-
-export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -7,15 +7,18 @@ import App from './App.tsx'
 import AppointmentForm from './modul/rezervovat.tsx'
 import Calendar from "./modul/Calendar.tsx";
 import Calendar1 from "./modul/Calendar1.tsx";
+import MyReservations from "./components/MyReservation.tsx";
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/rezervovat" element={<AppointmentForm />} />
-        <Route path="/calendar" element={<Calendar />} />
-        <Route path="/calendar1" element={<Calendar1 />} />
+        <Route path="moje-rezervace" element={<MyReservations />} />
+        <Route path="rezervovat" element={<AppointmentForm />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="calendar1" element={<Calendar1 />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
